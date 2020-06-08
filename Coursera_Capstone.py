32f58e07ea6f615a36a61ab9fce762c4f874ad1e
Panagiotis Erodotou <panagiotis.erod@gmail.com>
2020-06-08 01:02:56 +0100

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 00:56:40 2020
+
+@author: panagiotise
+"""
+
+"""
+I am using Anaconda Spyder for the capstone project of the coursera Aplied data science course
+"""
+
+import  pandas as pd
+import numpy as np
+
+
+print('Hello Capstone Project Course!')
+
+
+
